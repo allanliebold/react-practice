41210adf398804866dceea3e2166b8f28bfd2776
Allan Liebold <allan.liebold@gmail.com>
2017-09-15 08:32:56 -0700

    layout is now a separate component, imported into client.js

--- src/js/client.js
+++ src/js/client.js
@@ -1,21 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Layout extends React.Component {
-  saySomething() {
-    return 'Something!';
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>It works! Yay!</h1>
-        <p>These elements are wrapped in a div!</p>
-        <p>This one says: {this.saySomething()}</p>
-      </div>
-    );
-  }
-}
+import Layout from './components/layout';
 
 const app = document.getElementById('app');
 
---
+++ src/js/components/layout/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Layout extends React.Component {
+  saySomething() {
+    return 'Something!';
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>It works! Yay!</h1>
+        <p>These elements are wrapped in a div!</p>
+        <p>This one says: {this.saySomething()}</p>
+      </div>
+    );
+  }
+}
+
+export default Layout;
